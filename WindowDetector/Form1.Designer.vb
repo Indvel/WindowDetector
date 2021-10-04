4640cc5da73583e7249dba05e9dc683cee2840fd
fbtkd <fbtkd@DESKTOP-KBQQLQR>
2021-10-04 21:45:58 +0900

    초기 커밋.

---
+++ WindowDetector/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.labelId = New System.Windows.Forms.Label()
+        Me.labelName = New System.Windows.Forms.Label()
+        Me.labelSize = New System.Windows.Forms.Label()
+        Me.labelPath = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStart.Location = New System.Drawing.Point(75, 113)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(87, 31)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStop.Location = New System.Drawing.Point(168, 113)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(87, 31)
+        Me.btnStop.TabIndex = 1
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'labelId
+        '
+        Me.labelId.AutoSize = True
+        Me.labelId.Location = New System.Drawing.Point(12, 9)
+        Me.labelId.Name = "labelId"
+        Me.labelId.Size = New System.Drawing.Size(75, 12)
+        Me.labelId.TabIndex = 0
+        Me.labelId.Text = "ProcessID : "
+        '
+        'labelName
+        '
+        Me.labelName.AutoSize = True
+        Me.labelName.Location = New System.Drawing.Point(12, 30)
+        Me.labelName.Name = "labelName"
+        Me.labelName.Size = New System.Drawing.Size(51, 12)
+        Me.labelName.TabIndex = 0
+        Me.labelName.Text = "Name : "
+        '
+        'labelSize
+        '
+        Me.labelSize.AutoSize = True
+        Me.labelSize.Location = New System.Drawing.Point(12, 51)
+        Me.labelSize.Name = "labelSize"
+        Me.labelSize.Size = New System.Drawing.Size(42, 12)
+        Me.labelSize.TabIndex = 0
+        Me.labelSize.Text = "Size : "
+        '
+        'labelPath
+        '
+        Me.labelPath.Location = New System.Drawing.Point(12, 72)
+        Me.labelPath.Name = "labelPath"
+        Me.labelPath.Size = New System.Drawing.Size(304, 38)
+        Me.labelPath.TabIndex = 0
+        Me.labelPath.Text = "Path : "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(328, 158)
+        Me.Controls.Add(Me.labelPath)
+        Me.Controls.Add(Me.labelSize)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.labelName)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.labelId)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Window Detector"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents labelId As Label
+    Friend WithEvents labelName As Label
+    Friend WithEvents labelSize As Label
+    Friend WithEvents labelPath As Label
+End Class
---
+++ WindowDetector/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Drawing
+
+Public Class Form1
+
+    <DllImport("user32")>
+    Public Shared Function WindowFromPoint(pt As Point) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", EntryPoint:="GetWindowThreadProcessId")>
+    Private Shared Function GetWindowThreadProcessId(<InAttribute()> ByVal hWnd As IntPtr, ByRef lpdwProcessId As Integer) As Integer
+    End Function
+
+    <DllImport("user32")>
+    Private Shared Function GetCursorPos(ByRef pt As Point) As Int32
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True, CharSet:=CharSet.Auto)> Private Shared Function GetWindowTextLength(ByVal hwnd As IntPtr) As Integer
+    End Function
+
+    Public Declare Auto Function GetWindowText Lib "user32" _
+            (ByVal hWnd As System.IntPtr,
+            ByVal lpString As System.Text.StringBuilder,
+            ByVal cch As Integer) As Integer
+
+    Public Structure RECT
+        Public left As Integer
+        Public top As Integer
+        Public right As Integer
+        Public bottom As Integer
+    End Structure
+
+    Declare Function GetWindowRect Lib "user32" Alias "GetWindowRect" (ByVal hwnd As Integer, ByRef lpRect As RECT) As Integer
+
+    Declare Function GetClientRect Lib "user32" Alias "GetClientRect" (ByVal hwnd As Integer, ByRef lpRect As RECT) As Integer
+
+    <DllImport("user32.dll", SetLastError:=False)>
+    Private Shared Function GetDesktopWindow() As IntPtr
+    End Function
+
+    <DllImport("user32.dll")>
+    Public Shared Function GetDC(ByVal hwnd As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("user32.dll")>
+    Public Shared Function ReleaseDC(ByVal hWnd As IntPtr, ByVal hDC As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+    End Function
+
+    Public Overloads Declare Function InvalidateRect Lib "User32" Alias "InvalidateRect" (ByVal hWnd As IntPtr, ByRef lpRect As RECT, ByVal bErase As Boolean) As Boolean
+
+    Dim hwnd As IntPtr
+    Dim stroke As Pen = New Pen(Color.Red, 2)
+    Dim prevPid As Integer = Nothing
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim pos As Point
+        GetCursorPos(pos)
+        hwnd = WindowFromPoint(pos)
+
+        If hwnd <> IntPtr.Zero Then
+            Dim len As Integer = GetWindowTextLength(hwnd)
+            Dim title As New StringBuilder("", len + 1)
+            If len > 0 Then
+                GetWindowText(hwnd, title, title.Capacity)
+            End If
+            Dim pid As Integer = Nothing
+            GetWindowThreadProcessId(hwnd, pid)
+
+            Dim proc As Process = Process.GetProcessById(pid)
+            Dim fileName As String = ""
+            Try
+                fileName = proc.MainModule.FileName
+            Catch ex As Exception
+                fileName = ""
+            End Try
+
+            Dim rect As RECT
+            GetWindowRect(hwnd, rect)
+            If Not pid = prevPid Then
+                prevPid = pid
+                InvalidateRect(IntPtr.Zero, rect, True)
+            End If
+
+            Dim rta As Rectangle = New Rectangle(rect.left, rect.top, rect.right - rect.left, rect.bottom - rect.top)
+            Dim g As Graphics = Graphics.FromHwnd(IntPtr.Zero)
+            g.DrawRectangle(stroke, rta)
+            g.Dispose()
+
+            labelId.Text = "ProcessID : " & pid.ToString
+            labelName.Text = "Name : " & title.ToString
+            labelSize.Text = "Size : " & (rect.right - rect.left) & " X " & (rect.bottom - rect.top)
+            labelPath.Text = "Path : " & fileName.ToString
+        End If
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If Timer1.Enabled = False Then
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        If Timer1.Enabled Then
+            Timer1.Enabled = False
+        End If
+    End Sub
+End Class
